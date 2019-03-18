eb501624b6ee2da5d8f4e5a6b4d7816c02362475
aryehgigi <aryehgigi@gmail.com>
2019-03-18 15:56:45 +0200

    added propagate_gov_and_deps and collateral damage

--- converter.py
+++ converter.py
@@ -15,7 +15,7 @@ tag_counter = 0
 
 class Restriction(object):
         def __init__(self, dictionary):
-            self._dictionary = {"name": None, "gov": None, "diff": None, "form": None, "nested": None}
+            self._dictionary = {"name": None, "gov": None, "no-gov": None, "diff": None, "form": None, "nested": None}
             self._dictionary.update(dictionary)
         
         def __setitem__(self, key, item):
@@ -27,69 +27,85 @@ class Restriction(object):
             return self._dictionary[key]
 
 
-def match(sent, cur_id, cur_name, restriction_list, given_named_nodes=None):
-    is_matched = False
-    named_nodes = dict()
-    if cur_name:
-        named_nodes[cur_name] = sent[cur_id]
-    
-    for restriction in restriction_list:
-        # first validate the restriction is not None,
-        # this might happen if we build CONDITIONED restrictions.
-        if not restriction:
-            continue
+# ----------------------------------------- matching functions ----------------------------------- #
+
+
+def match(sent, cur_id, cur_name, restriction_lists, given_named_nodes=None):
+    for restriction_list in restriction_lists:
+        named_nodes = dict()
+        if cur_name:
+            named_nodes[cur_name] = sent[cur_id]
         
-        is_matched = False
-        for child in sent[cur_id]['children_list']:
-            inner_named_nodes = {}
-            if restriction["gov"]:
-                if not re.match(restriction["gov"], child['conllu_info'].deprel):
-                    continue
+        one_restriction_violated = False
+        for restriction in restriction_list:
+            # first validate the restriction is not None,
+            # this might happen if we build CONDITIONED restrictions.
+            if not restriction:
+                continue
             
-            if restriction["nested"]:
-                inner_is_matched, inner_named_nodes = match(sent, child['conllu_info'].id, restriction["name"], restriction["nested"], named_nodes)
-                if not inner_is_matched:
-                    continue
-            
-            if restriction["diff"]:
-                found = False
-                for diff in restriction["diff"]:
-                    named_child = None
-                    if diff in named_nodes:
-                        named_child = named_nodes[diff]
-                    elif given_named_nodes and diff in given_named_nodes:
-                        named_child = given_named_nodes[diff]
-                    # In the beginning I thought this counts as a bug,
-                    # But it can be used as a diff if we found a previous match and assigned it a name.
-                    # else:
-                    #     raise Exception("Restriction of unknown named node: " + restriction["diff"] + " ".join(named_nodes)
-                    #                     + (" ".join(given_named_nodes) if given_named_nodes else ""))
-                    
-                    if named_child and not (named_child != child):
-                        found = True
-                if found:
-                    continue
-            
-            if restriction["form"]:
-                if not re.match(restriction["form"], child['conllu_info'].form):
-                    continue
-            
-            if restriction["name"]:
-                named_nodes[restriction["name"]] = child
- 
-            named_nodes.update(inner_named_nodes)
-            is_matched = True
-            break
+            restriction_matched = False
+            for child in sent[cur_id]['children_list']:
+                inner_named_nodes = {}
+                if restriction["gov"]:
+                    if not re.match(restriction["gov"], child['conllu_info'].deprel):
+                        continue
                 
-        if not is_matched:
-            break
-    
-    return is_matched, named_nodes
+                if restriction["no-gov"]:
+                    if re.match(restriction["no-gov"], child['conllu_info'].deprel):
+                        break
+                
+                if restriction["nested"]:
+                    inner_is_matched, inner_named_nodes = match(sent, child['conllu_info'].id, restriction["name"], restriction["nested"], named_nodes)
+                    
+                    if not inner_is_matched:
+                        continue
+                
+                if restriction["diff"]:
+                    found = False
+                    for diff in restriction["diff"]:
+                        named_child = None
+                        if diff in named_nodes:
+                            named_child = named_nodes[diff]
+                        elif given_named_nodes and diff in given_named_nodes:
+                            named_child = given_named_nodes[diff]
+                        # In the beginning I thought this counts as a bug,
+                        # But it can be used as a diff if we found a previous match and assigned it a name.
+                        # else:
+                        #     raise Exception("Restriction of unknown named node: " + restriction["diff"] + " ".join(named_nodes)
+                        #                     + (" ".join(given_named_nodes) if given_named_nodes else ""))
+                        
+                        if named_child and not (named_child != child):
+                            found = True
+                    if found:
+                        continue
+                
+                if restriction["form"]:
+                    if not re.match(restriction["form"], child['conllu_info'].form):
+                        continue
+                
+                if restriction["name"]:
+                    named_nodes[restriction["name"]] = child
+     
+                named_nodes.update(inner_named_nodes)
+                restriction_matched = True
+                break
+            
+            if not restriction_matched:
+                one_restriction_violated = True
+                break
+        
+        if not one_restriction_violated:
+            return True, named_nodes
+        
+    return False, None
 
 
-def add_edge(node, new_rel, new_spec=None):
+# ----------------------------------------- replacing functions ---------------------------------- #
+
+
+def add_edge(node, new_rel, new_spec=None, head=None):
     global tag_counter
-    head_id = node['conllu_info'].head
+    head_id = node['conllu_info'].head if not head else head
     rel_with_specs = new_rel + ((":" + new_spec) if new_spec else "")
     if head_id in node['new_deps']:
         # TODO - this is not the ideal result, we want to have multigraph with same head here,
@@ -118,12 +134,15 @@ def replace_edge(node, new_spec_list=None, new_rel=None, old_rel=None):
         add_edge(node, new_rel)
 
 
+# ----------------------------------------- content functions ------------------------------------ #
+
+
 def correct_subj_pass(sentence):
     for (cur_id, token) in sentence.items():
-        is_matched, ret = match(sentence, cur_id, None, [
+        is_matched, ret = match(sentence, cur_id, None, [[
             Restriction({"gov": 'auxpass'}),
             Restriction({"gov": "^(nsubj|csubj).*$", "name": "subj"})
-        ])
+        ]])
         if not is_matched:
             continue
         
@@ -132,15 +151,15 @@ def correct_subj_pass(sentence):
 
 def passive_agent(sentence):
     for (cur_id, token) in sentence.items():
-        is_matched, ret = match(sentence, cur_id, None, [
-            Restriction({"gov": 'nmod', "name": "mod", "nested": [
+        is_matched, ret = match(sentence, cur_id, None, [[
+            Restriction({"gov": 'nmod', "name": "mod", "nested": [[
                 Restriction({"gov": 'case', "form": "^(?i:by)$"})
-            ]}),
+            ]]}),
             Restriction({"gov": "auxpass"})
-        ])
+        ]])
         if not is_matched:
             continue
-        
+        import pdb;pdb.set_trace()
         replace_edge(ret['mod'], new_spec_list=["agent"])
 
 
@@ -157,14 +176,14 @@ def prep_patterns(sentence, used_cased, mw2=False, mw3=False, fix_nmods=True):
         second_gov = '^(mark|case)$'
     
     for (cur_id, token) in sentence.items():
-        is_matched, ret = match(sentence, cur_id, None, [
-            Restriction({"gov": first_gov, "name": "mod", "nested": [
-                Restriction({"gov": second_gov, "name": "c1", "nested": [
+        is_matched, ret = match(sentence, cur_id, None, [[
+            Restriction({"gov": first_gov, "name": "mod", "nested": [[
+                Restriction({"gov": second_gov, "name": "c1", "nested": [[
                     Restriction({"gov": 'mwe', "name": "c2"}),
                     Restriction({"gov": 'mwe', "name": "c3", "diff": ["c2"]}) if mw3 else None
-                ] if mw2 or mw3 else None})
-            ]})
-        ])
+                ]] if mw2 or mw3 else None})
+            ]]})
+        ]])
         
         # We only want to match every case/marker once, so we test, and save them for further propagation
         # TODO - maybe a nicer and clearer way to do this?
@@ -200,9 +219,9 @@ def conj_help_matcher(sentence, cur_id, ret_conj, matcher):
     i = 1
     given_named_nodes = {}
     while is_matched:
-        is_matched, ret = match(sentence, cur_id, None, [
+        is_matched, ret = match(sentence, cur_id, None, [[
             Restriction({"gov": matcher, "name": str(i) + matcher, "diff": given_named_nodes.keys()})
-        ], given_named_nodes=given_named_nodes)
+        ]], given_named_nodes=given_named_nodes)
         if is_matched:
             ret_conj.append((ret[str(i) + matcher]["conllu_info"].id, ret[str(i) + matcher]))
             given_named_nodes[str(i) + matcher] = ret[str(i) + matcher]
@@ -227,6 +246,33 @@ def conj_info(sentence):
                 replace_edge(conj, new_spec_list=[cur_form])
 
 
+def propagate_gov_and_deps(sentence):
+    for (cur_id, token) in sentence.items():
+        is_matched, ret = match(sentence, cur_id, "gov_new", [
+            [
+                Restriction({"gov": "[^(root|case|nsubj|dobj)]", "name": "gov", "nested": [
+                    [
+                        Restriction({"gov": "rcmod"}),
+                        Restriction({"gov": ".*conj.*", "name": "dep", "diff": ["gov_new"]})
+                    ],
+                    [
+                        Restriction({"gov": ".*conj.*", "name": "dep", "diff": ["gov_new"], "nested": [[Restriction({"gov": "rcmod"})]]})
+                    ]
+                ]})
+            ],
+            [
+                Restriction({"gov": "[^(root|case)]", "name": "gov", "nested": [[
+                    Restriction({"no-gov": "rcmod"}),
+                    Restriction({"gov": ".*conj.*", "name": "dep", "diff": ["gov_new"], "nested": [[Restriction({"no-gov": "rcmod"})]]})
+                ]]})
+            ]])
+            
+        if not is_matched:
+            continue
+        
+        add_edge(ret['dep'], ret['gov']['conllu_info'].deprel, head=ret['gov']['conllu_info'].head)
+
+
 def convert_sentence(sentence):
     # correctDependencies - correctSubjPass
     correct_subj_pass(sentence)
@@ -244,6 +290,10 @@ def convert_sentence(sentence):
     
     # addConjInformation
     conj_info(sentence)
+    
+    # treatCC
+    propagate_gov_and_deps(sentence)
+    
     # TODO - continue conversion
     return sentence
 
