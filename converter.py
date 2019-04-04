ebf7b97b76ef65e14b8a7909682794046cb53ff8
aryehgigi <aryehgigi@gmail.com>
2019-04-04 09:05:04 +0300

    ne matcher module

--- converter.py
+++ converter.py
@@ -1,4 +1,6 @@
 import regex as re
+
+from matcher import match, Restriction
 import configuration as conf
 
 # constants
@@ -11,98 +13,6 @@ w1_quant_mod_of_2w = "(?i:lots|many|several|plenty|tons|dozens|multitudes|mounta
 w1_quant_mod_of_2w_det = "(?i:some|all|both|neither|everyone|nobody|one|two|three|four|five|six|seven|eight|nine|ten|hundred|thousand|million|billion|trillion|[0-9]+)"
 relativizing_word_regex = "(?i:that|what|which|who|whom|whose)"
 
-
-class Restriction(object):
-    def __init__(self, dictionary):
-        self._dictionary = {"name": None, "gov": None, "no-gov": None, "diff": None,
-                            "form": None, "xpos": None, "follows": None, "followed_by": None, "nested": None}
-        self._dictionary.update(dictionary)
-    
-    def __setitem__(self, key, item):
-        if key not in self._dictionary:
-            raise KeyError("The key {} is not defined.".format(key))
-        self._dictionary[key] = item
-    
-    def __getitem__(self, key):
-        return self._dictionary[key]
-
-
-# ----------------------------------------- matching functions ----------------------------------- #
-
-def neighbors_restrictions(restriction, child, named_nodes):
-    if restriction["follows"]:
-        follows, _, _ = named_nodes[restriction["follows"]]
-        if child.get_conllu_field('id') - 1 != follows.get_conllu_field('id'):
-            return False
-    
-    if restriction["followed"]:
-        followed, _, _ = named_nodes[restriction["followed"]][0]
-        if child.get_conllu_field('id') + 1 != followed.get_conllu_field('id'):
-            return False
-    return True
-
-
-def match(children, restriction_lists, head=None):
-    ret = []
-    for restriction_list in restriction_lists:
-        one_restriction_violated = False
-        for restriction in restriction_list:
-            restriction_matched = False
-            for child in children:
-                if restriction["form"]:
-                    if not re.match(restriction["form"], child.get_conllu_field('form')):
-                        continue
-                
-                if restriction["xpos"]:
-                    if not re.match(restriction["xpos"], child.get_conllu_field('xpos')):
-                        continue
-                
-                relations = [None]
-                if restriction["gov"]:
-                    relations = child.match_rel(restriction["gov"], head)
-                    if len(relations) == 0:
-                        continue
-                elif head:
-                    relations = [b for a, b in child.get_new_relations(head)]
-                
-                if restriction["no-gov"]:
-                    if False in [len(grandchild.match_rel(restriction["no-gov"], child)) == 0 for grandchild in child.get_children()]:
-                        continue
-                
-                nested = []
-                if restriction["nested"]:
-                    nested = match(
-                        child.get_children(),
-                        restriction["nested"],
-                        head=child)
-                    
-                    nested = [named_nodes for named_nodes in nested
-                              if neighbors_restrictions(restriction, child, named_nodes)]
-                    
-                    if not nested:
-                        continue
-                
-                if restriction["name"]:
-                    for rel in relations:
-                        if nested:
-                            for d in nested:
-                                d[restriction["name"]] = (child, head, rel)
-                                ret.append(d)
-                        else:
-                            ret.append(dict({restriction["name"]: (child, head, rel)}))
-                else:
-                    ret = nested
-                restriction_matched = True
-            
-            if not restriction_matched:
-                one_restriction_violated = True
-                break
-        
-        if not one_restriction_violated:
-            return ret
-    
-    return ret
-    
 
 # corrects subjs (includes nsubj/csubj/nsubj:xsubj/csubj:xsubj) to subjpass,
 # if they are a sibling of auxpass.
