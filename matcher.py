02aa0b988496dcdcc9ed16d2282918aec9cfead4
aryehgigi <aryehgigi@gmail.com>
2019-04-04 09:48:01 +0300

    breakdown match function

--- matcher.py
+++ matcher.py
@@ -15,7 +15,9 @@ class Restriction(object):
     def __getitem__(self, key):
         return self._dictionary[key]
 
+
 # ----------------------------------------- matching functions ----------------------------------- #
+
 
 def neighbors_restrictions(restriction, child, named_nodes):
     if restriction["follows"]:
@@ -29,64 +31,88 @@ def neighbors_restrictions(restriction, child, named_nodes):
             return False
     return True
 
-def match(children, restriction_lists, head=None):
-    ret = []
-    for restriction_list in restriction_lists:
-        one_restriction_violated = False
-        for restriction in restriction_list:
-            restriction_matched = False
-            for child in children:
-                if restriction["form"]:
-                    if not re.match(restriction["form"], child.get_conllu_field('form')):
-                        continue
-                
-                if restriction["xpos"]:
-                    if not re.match(restriction["xpos"], child.get_conllu_field('xpos')):
-                        continue
-                
-                relations = [None]
-                if restriction["gov"]:
-                    relations = child.match_rel(restriction["gov"], head)
-                    if len(relations) == 0:
-                        continue
-                elif head:
-                    relations = [b for a, b in child.get_new_relations(head)]
-                
-                if restriction["no-gov"]:
-                    if False in [len(grandchild.match_rel(restriction["no-gov"], child)) == 0 for grandchild in
-                                 child.get_children()]:
-                        continue
-                
-                nested = []
-                if restriction["nested"]:
-                    nested = match(
-                        child.get_children(),
-                        restriction["nested"],
-                        head=child)
-                    
-                    nested = [named_nodes for named_nodes in nested
-                              if neighbors_restrictions(restriction, child, named_nodes)]
-                    
-                    if not nested:
-                        continue
-                
-                if restriction["name"]:
-                    for rel in relations:
-                        if nested:
-                            for d in nested:
-                                d[restriction["name"]] = (child, head, rel)
-                                ret.append(d)
-                        else:
-                            ret.append(dict({restriction["name"]: (child, head, rel)}))
-                else:
-                    ret = nested
-                restriction_matched = True
-            
-            if not restriction_matched:
-                one_restriction_violated = True
-                break
+
+def match_child(child, restriction, head):
+    if restriction["form"]:
+        if not re.match(restriction["form"], child.get_conllu_field('form')):
+            return []
+    
+    if restriction["xpos"]:
+        if not re.match(restriction["xpos"], child.get_conllu_field('xpos')):
+            return []
+    
+    relations = [None]
+    if restriction["gov"]:
+        relations = child.match_rel(restriction["gov"], head)
+        if len(relations) == 0:
+            return []
+    elif head:
+        relations = [b for a, b in child.get_new_relations(head)]
+    
+    if restriction["no-gov"]:
+        if False in [len(grandchild.match_rel(restriction["no-gov"], child)) == 0 for grandchild in
+                     child.get_children()]:
+            return []
+    
+    nested = []
+    if restriction["nested"]:
+        nested = match(
+            child.get_children(),
+            restriction["nested"],
+            head=child)
         
-        if not one_restriction_violated:
-            return ret
+        nested = [named_nodes for named_nodes in nested
+                  if neighbors_restrictions(restriction, child, named_nodes)]
+        
+        if not nested:
+            return []
+    
+    if restriction["name"]:
+        ret = []
+        for rel in relations:
+            if nested:
+                for d in nested:
+                    d[restriction["name"]] = (child, head, rel)
+                    ret.append(d)
+            else:
+                ret.append(dict({restriction["name"]: (child, head, rel)}))
+    else:
+        return nested
+    
+
+def match_rest(children, restriction, head):
+    ret = []
+    for child in children:
+        ret += match_child(child, restriction, head)
     
     return ret
+
+
+def match_rl(children, restriction_list, head):
+    ret = []
+    rest_rets = []
+    for restriction in restriction_list:
+        rest_ret = match_rest(children, restriction, head)
+        
+        # if one restriction was violated, return empty list.
+        if not rest_ret:
+            return []
+        
+        rest_rets.append(rest_ret)
+    
+    # every new rest_ret should be merged to any previous rest_ret
+    # we do this here because we are lazy -
+    # we don't want to do this work if one restriction was violated.
+    for rest_ret in rest_rets:
+        ret = rest_ret if not ret else \
+            [{**ns_ret, **ns_rest_ret} for ns_rest_ret in rest_ret for ns_ret in ret]
+    
+    return ret
+    
+
+def match(children, restriction_lists, head=None):
+    for restriction_list in restriction_lists:
+        ret = match_rl(children, restriction_list, head)
+        if ret:
+            return ret
+    return []
