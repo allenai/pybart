a4e5ec50971294ca30e80dfb0c78330f69da7a3f
aryehgigi <aryehgigi@gmail.com>
2019-05-16 18:25:51 +0300

    added hackes for color and replace-on-collison

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -14,7 +14,8 @@ define([
 	function main() {
 		
 		function displayTree(data, containerId, category) {
-			const container = $('#' + containerId);
+			var links = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : [];
+            const container = $('#' + containerId);
 			const basicTag = TAG.tag({
 				// The `container` parameter can take either the ID of the main element or
 				// the main element itself (as either a jQuery or native object)
@@ -34,15 +35,67 @@ define([
 				  showTopMainLabel: true,
                   showTopLinksOnMove: true,
 				  showTopArgLabels: false,
+				  showBottomMainLabel: true,
+                  showBottomLinksOnMove: true,
+				  showBottomArgLabels: false,
 				  topLinkCategory: category,
+				  BottomLinkCategory: category,
 				  topTagCategory: "none",
 				  bottomTagCategory: "POS",
-                  showBottomMainLabel: false,
-                  rowVerticalPadding: 2,
+                  //rowVerticalPadding: 2,
+                  compactRows: true,
+                  
 				}
 			});
             basicTag.parser._parsedData.links.forEach((e) => {e.top = true})
             basicTag.redraw()
+            if (links.length > 0)
+            {
+                basicTag.links.forEach((e) => {
+                    found = false
+                    links.forEach((e2) => {
+                        if ((e2.arguments[0].anchor.text == e.arguments[0].anchor.text) && (e2.trigger.text == e.trigger.text))
+                        {
+                            found = true
+                            if (e2.reltype != e.reltype)
+                            {
+                                e.svg.node.style.fill = "#FF7800"
+                                e2.svg.node.style.fill = "#FF7800"
+                            }
+                        }
+                    })
+
+                    if (found == false)
+                    {
+                        e.svg.node.style.fill = "#00FF00"
+                        basicTag.links.forEach((e3) => {
+                            if ((e != e3) && ((((e3.arguments[0].anchor.text == e.arguments[0].anchor.text) && (e3.trigger.text == e.trigger.text))) || (((e3.arguments[0].anchor.text == e.trigger.text) && (e3.trigger.text == e.arguments[0].anchor.text)))))
+                            {
+                                e.top = false
+                                e.slot = e.slot * -1
+                                e.show()
+                                basicTag.resize()
+                            }
+                        })
+                    }
+                    
+                })
+                links.forEach((e) => {
+                    found = false
+                    basicTag.links.forEach((e2) => {
+                        if ((e2.arguments[0].anchor.text == e.arguments[0].anchor.text) && (e2.trigger.text == e.trigger.text))
+                        {
+                            found = true
+                        }
+                    })
+
+                    if (found == false)
+                    {
+                        e.svg.node.style.fill = "#FF0000"
+                    }
+                })
+            }
+            return basicTag
 		}
 		
 		const $submitButton = $("#submitButton");
@@ -56,8 +109,8 @@ define([
             $('#containerBasic').empty()
             $('#containerPlus').empty()
             
-			displayTree(response.data.basic, "containerBasic", "universal-basic");
-			displayTree(response.data.plus, "containerPlus", "universal-plus");
+			tag1 = displayTree(response.data.basic, "containerBasic", "universal-basic");
+			tag2 = displayTree(response.data.plus, "containerPlus", "universal-plus", tag1.links);
 		});
 	}
 	
