106ee0b54688645bf22471b1f6e0c2d87d3e5515
aryehgigi <aryehgigi@gmail.com>
2019-07-17 11:19:01 +0300

    make more genere

--- compare_ud_types.py
+++ compare_ud_types.py
@@ -1,3 +1,4 @@
+import os
 from bottle import route, run, request, static_file
 import json
 import spacy
@@ -5,14 +6,15 @@ from spacy_conll import Spacy2ConllParser
 import converter
 import conllu_wrapper as cw
 
-ARBITRARY_PATH = "c:/temp/sentence.txt"
+ARBITRARY_PATH = "sentence.txt"
 
 
 @route('/')
 @route('/<filepath:path>')
 def server_static(filepath="index.html"):
-    if ("/" not in filepath) and (filepath != "index.html") and (filepath != "favicon.ico"):
-        filepath = "index.html"
+    lastpart = filepath[filepath.rfind('/') + 1:]
+    if (" " in lastpart) or ("." not in lastpart):
+        filepath = filepath.replace(lastpart, "index.html")
     return static_file(filepath, root='./public/')
 
 
@@ -29,6 +31,7 @@ def annotate():
     spacyconll.parseprint(input_str=sentence, output_file=ARBITRARY_PATH, is_tokenized=True)
     with open(ARBITRARY_PATH, "r") as f:
         conllu_basic_out = f.read()
+    os.remove(ARBITRARY_PATH)
     
     conllu_basic_out_formatted, _ = cw.parse_conllu(conllu_basic_out)
     conllu_plus_out_formatted = converter.convert(cw.parse_conllu(conllu_basic_out)[0], eud, False, eud_pp, eud_aryeh)
