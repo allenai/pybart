05597cce9b1ef70d35a4a364f1214ad8215fd446
aryehgigi <aryehgigi@gmail.com>
2019-09-09 11:50:40 +0300

    hirarchy2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ud2ude-pkg-aryehgigi",
+    name="ud2ude_aryehgigi",
     version="0.0.1",
     author="Aryeh Tiktinsky",
     author_email="aryehgigi@gmail.com",
