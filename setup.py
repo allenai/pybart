cde0b2daf2bec0106c913fd78b763cf7b318e624
aryehgigi <aryehgigi@gmail.com>
2019-12-10 00:04:23 +0200

    prepare for real name in pypi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="ud2ude-aryehgigi",
+    name="ud2ude",
     version="2.1.0",
     author="Aryeh Tiktinsky",
     author_email="aryehgigi@gmail.com",
     description="python converter from UD to enhanced UD representations",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/aryehgigi/UD2UDE",
+    url="https://github.com/allenai/UD2UDE",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
