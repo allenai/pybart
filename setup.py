fe679582f02d97e5095b7383bf3aa339493d31ce
aryehgigi <aryehgigi@gmail.com>
2020-01-01 13:43:06 +0200

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ud2ude",
-    version="2.1.0",
+    version="2.2.0",
     author="Aryeh Tiktinsky",
     author_email="aryehgigi@gmail.com",
     description="python converter from UD to enhanced UD representations",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.7',
 )
