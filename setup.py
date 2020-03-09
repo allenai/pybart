6a9fea07c47d73a69e2831ff9d46714fc1257765
aryehgigi <aryehgigi@gmail.com>
2020-03-09 14:41:38 +0200

    update to version 2.2.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ud2ude",
-    version="2.2.1",
+    version="2.2.2",
     author="Aryeh Tiktinsky",
     author_email="aryehgigi@gmail.com",
     description="python converter from UD to enhanced UD representations",
