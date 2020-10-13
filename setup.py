708a39aa40a36b974d1df9662a3e629daeeb971c
aryeht <aryehgigi@gmail.com>
2020-10-13 10:40:42 +0300

    fix python requirements for package

--- setup.py
+++ setup.py
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.7',
+    python_requires='>=3.7, <3.9',
 )
