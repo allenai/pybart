d69e122b88a2bf6be64c233f6007c020cf9670ac
aryeht <aryehgigi@gmail.com>
2021-03-22 03:07:18 +0200

    add support for spacy3

--- pybart/api.py
+++ pybart/api.py
@@ -2,6 +2,8 @@ import math
 
 from .conllu_wrapper import parse_conllu, serialize_conllu, parse_odin, conllu_to_odin, parsed_tacred_json
 from .converter import convert, get_conversion_names as inner_get_conversion_names, init_conversions
+from spacy.language import Language
+from .spacy_wrapper import parse_spacy_sent, enhance_to_spacy_doc
 
 
 def convert_bart_conllu(conllu_text, enhance_ud=True, enhanced_plus_plus=True, enhanced_extra=True, preserve_comments=False, conv_iterations=math.inf, remove_eud_info=False, remove_extra_info=False, remove_node_adding_conversions=False, remove_unc=False, query_mode=False, funcs_to_cancel=None, ud_version=1):
@@ -22,22 +24,21 @@ def convert_bart_odin(odin_json, enhance_ud=True, enhanced_plus_plus=True, enhan
             odin_json["documents"][doc_key] = _convert_bart_odin_sent(doc, enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version)
     else:
         odin_json = _convert_bart_odin_sent(odin_json, enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version)
-    
+
     return odin_json
 
 
 def convert_bart_tacred(tacred_json, enhance_ud=True, enhanced_plus_plus=True, enhanced_extra=True, conv_iterations=math.inf, remove_eud_info=False, remove_extra_info=False, remove_node_adding_conversions=False, remove_unc=False, query_mode=False, funcs_to_cancel=None, ud_version=1):
     sents = parsed_tacred_json(tacred_json)
     converted_sents, _ = convert(sents, enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version)
-    
+
     return converted_sents
 
 
 def convert_spacy_doc(doc, enhance_ud=True, enhanced_plus_plus=True, enhanced_extra=True, conv_iterations=math.inf, remove_eud_info=False, remove_extra_info=False, remove_node_adding_conversions=False, remove_unc=False, query_mode=False, funcs_to_cancel=None, ud_version=1, one_time_initialized_conversions=None):
-    from .spacy_wrapper import parse_spacy_sent, serialize_spacy_doc
     parsed_doc = [parse_spacy_sent(sent) for sent in doc.sents]
     converted, convs_done = convert(parsed_doc, enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version, one_time_initialized_conversions)
-    return serialize_spacy_doc(doc, converted), converted, convs_done
+    return enhance_to_spacy_doc(doc, converted), converted, convs_done
 
 
 class Converter:
@@ -45,19 +46,35 @@ class Converter:
         self.config = (enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version)
         # make conversions and (more importantly) constraint initialization, a one timer.
         self.conversions = init_conversions(remove_node_adding_conversions, ud_version)
-    
+
     def __call__(self, doc):
         serialized_spacy_doc, converted_sents, convs_done = convert_spacy_doc(doc, *self.config, self.conversions)
         self._converted_sents = converted_sents
         self._convs_done = convs_done
         return serialized_spacy_doc
-    
+
     def get_converted_sents(self):
         return self._converted_sents
-    
+
     def get_max_convs(self):
         return self._convs_done
 
 
+class ConverterWithNlp(Converter):
+    def __init__(self, nlp, enhance_ud=True, enhanced_plus_plus=True, enhanced_extra=True,
+                 conv_iterations=math.inf, remove_eud_info=False, remove_extra_info=False,
+                 remove_node_adding_conversions=False, remove_unc=False, query_mode=False, funcs_to_cancel=None,
+                 ud_version=1):
+        super().__init__(enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version)
+
+
 def get_conversion_names():
     return inner_get_conversion_names()
+
+
+@Language.factory(
+   "pybart_spacy_pipe",
+   default_config={"enhance_ud": True, "enhanced_plus_plus": True, "enhanced_extra": True, "conv_iterations": math.inf, "remove_eud_info": False, "remove_extra_info": False, "remove_node_adding_conversions": False, "remove_unc": False, "query_mode": False, "funcs_to_cancel": None, "ud_version": 1, "one_time_initialized_conversions": None},
+)
+def create_pybart_spacy_pipe(nlp, name, enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version):
+    return ConverterWithNlp(nlp, enhance_ud, enhanced_plus_plus, enhanced_extra, conv_iterations, remove_eud_info, remove_extra_info, remove_node_adding_conversions, remove_unc, query_mode, funcs_to_cancel, ud_version)
--- pybart/spacy_wrapper.py
+++ pybart/spacy_wrapper.py
@@ -1,15 +1,31 @@
 import struct
+from typing import Any, Dict
+
 from spacy.tokens import Doc, Token as SpacyToken
-from spacy import attrs
-import numpy as np
+from spacy.tokens.graph import Graph
 
 from .graph_token import Token, add_basic_edges, TokenId
 
+JsonObject = Dict[str, Any]
 NUM_OF_BITS = struct.calcsize("P") * 8
 
 
 # this is here because it needs to happen only once (per import)
-SpacyToken.set_extension("parent_list", default=[])
+SpacyToken.set_extension("parent_graphs_per_sent", default=[])
+
+
+def enhance_spike_doc(doc: Doc, spike_doc: JsonObject) -> JsonObject:
+    spike_doc["graphs"]["universal-enhanced"] = {"edges": [], "roots": []}
+    for edge in doc._.parent_graph.edges():
+        if edge.label_.lower().startswith("root"):
+            spike_doc["graphs"]["universal-enhanced"]["roots"].append(edge.tail.tokens[0].i)  # assume we have only one token per graph node
+        else:
+            spike_doc["graphs"]["universal-enhanced"]["edges"].append(
+                {"source": edge.head.tokens[0].i, "destination": edge.tail.tokens[0].i, "relation": edge.label_})
+    return spike_doc
+
+
+Doc.set_extension("enhance_spike_doc", method=enhance_spike_doc)
 
 
 def parse_spacy_sent(sent):
@@ -42,76 +58,25 @@ def parse_bart_label(rel, is_state_head_node):
     return rel.with_no_bart(), src, bool(rel.uncertain), rel.iid
 
 
-def serialize_spacy_doc(orig_doc, converted_sentences):
-    words = []
-    spaces = []
-    total_attrs = []
-    attrs_ = list(attrs.NAMES)
-    attrs_.remove('SENT_START')  # this clashes HEAD (see spacy documentation)
-    attrs_.remove('SPACY')  # we dont want to override the spaces we assign later on
-    
+def enhance_to_spacy_doc(orig_doc, converted_sentences):
+    offset = 0
     for orig_span, converted_sentence in zip(orig_doc.sents, converted_sentences):
-        # remove redundant dummy-root-node
-        orig = orig_span.as_doc()
-
-        # get attributes of original doc
-        orig_attrs = orig.to_array(attrs_)
-        
-        # append copied attributes for new nodes
-        new_nodes_attrs = []
-        for tok in converted_sentence:
-            iid = tok.get_conllu_field("id")
-            if iid.minor:
-                new_node_attrs = list(orig_attrs[iid.major - 1])
-                
-                # here we fix the relative head he is pointing to,
-                # in case it is a negative number we need to cast it to its unsigned synonym
-                relative = iid.major - (len(orig_attrs) + len(new_nodes_attrs) + 1)
-                new_node_attrs[attrs_.index('HEAD')] = relative + (2**NUM_OF_BITS if relative < 0 else 0)
-                
-                new_nodes_attrs.append(new_node_attrs)
-        if new_nodes_attrs:
-            new_attrs = np.append(orig_attrs, new_nodes_attrs, axis=0)
-        else:
-            new_attrs = orig_attrs
-        total_attrs = np.append(total_attrs, new_attrs, axis=0) if len(total_attrs) > 0 else new_attrs
-        
-        # fix whitespaces in case of new nodes: take original spaces. change the last one if there are new nodes.
-        #   add spaces for each new nodes, except for last
-        spaces += [t.whitespace_ if not ((i + 1 == len(orig)) and (len(new_nodes_attrs) > 0)) else ' ' for i, t in enumerate(orig)] + \
-                  [' ' if i + 1 < len(converted_sentence) else '' for i, t in enumerate(converted_sentence) if t.get_conllu_field("id").minor]
-        spaces[-1] = ' '
-        words += [t.get_conllu_field("form") for t in converted_sentence]
-    
-    # form new doc including new nodes and set attributes
-    spaces[-1] = ''
-    new_doc = Doc(orig_doc.vocab, words=words, spaces=spaces)
-    new_doc.from_array(attrs_, total_attrs)
-    
-    j = 0
-    for converted_sentence in converted_sentences:
-        # store spacy ids for head indices extraction later on
-        spacy_ids = {str(tok.get_conllu_field("id")): (spacy_i + j) for spacy_i, tok in enumerate(converted_sentence)}
-        
-        # set new info for all tokens per their head lists
-        for i, bart_tok in enumerate(converted_sentence):
-            spacy_tok = new_doc[i + j]
-            for head, rels in bart_tok.get_new_relations():
+        node_indices_map = dict()
+        nodes = []
+        edges = []
+        labels = []
+        for idx, tok in enumerate(converted_sentence):
+            node_indices_map[(tok.new_id.major, tok.new_id.minor)] = idx
+            nodes.append((tok.new_id.major - 1 + offset,) if tok.new_id.minor == 0 else ())
+        for idx, tok in enumerate(converted_sentence):
+            for head, rels in tok.get_new_relations():
                 for rel in rels:
-                    # extract spacy correspondent head id
-                    head_tok = new_doc[spacy_ids[str(head.get_conllu_field("id"))] if head.get_conllu_field("id").major != 0 else spacy_tok.i]
-                    # parse stringish label
-                    is_state_head_node = ((head_tok.text == "STATE") and head.get_conllu_field("id").minor) or \
-                                         bart_tok.get_conllu_field("id").minor
-                    new_rel, src, unc, alt = parse_bart_label(rel, is_state_head_node=is_state_head_node)
-                    # add info to token
-                    spacy_tok._.parent_list.append({'head': head_tok.i, 'rel': new_rel, 'src': src, 'alt': alt, 'unc': unc})
-            
-            # fix sentence boundaries, need to turn off is_parsed bool as it prevents setting the boundaries
-            new_doc.is_parsed = False
-            spacy_tok.is_sent_start = False if i != 0 else True
-            new_doc.is_parsed = True
-        
-        j += len(converted_sentence)
-    
-    return new_doc
+                    edges.append((
+                        node_indices_map[(head.new_id.major, head.new_id.minor)],
+                        node_indices_map[(tok.new_id.major, tok.new_id.minor)]
+                    ))
+                    _ = orig_doc.vocab[rel.to_str()]  # this will push the label into the vocab if it's not there
+                    labels.append(rel.to_str())
+
+        orig_doc._.parent_graphs_per_sent.append(Graph(orig_doc, name="pybart", nodes=nodes, edges=edges, labels=labels))
+        offset += len(orig_span)
