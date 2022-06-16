6d7693994cb48be54af25fd1cdff24753515b5cd
aryeht <aryehgigi@gmail.com>
2022-06-16 14:54:34 +0300

    update version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pybart-nlp",
-    version="3.2.9",
+    version="3.2.10",
     author="Aryeh Tiktinsky",
     author_email="aryehgigi@gmail.com",
     description="python converter from UD-tree to BART-graph representations",
