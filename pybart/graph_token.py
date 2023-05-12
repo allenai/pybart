2c8b1246151e87eb10779d48499f9971d613eec8
aryeht <aryehgigi@gmail.com>
2023-05-12 07:13:21 +0300

    fixes

--- pybart/graph_token.py
+++ pybart/graph_token.py
@@ -39,7 +39,11 @@ class Label:
                 dep_args = ", ".join(x for x in filter(None, [self.src_type, self.phrase, "UNC" if self.uncertain else None]))
                 bart = "@" + self.src + "(" + dep_args + ")" + iid_str
 
-        return self.base + eud + bart
+        # special cases for altering the base label
+        base = self.base
+        if self.src == "compound" and self.src_type == "NULL":
+            base += "c"
+        return base + eud + bart
 
     # operator overloading: less than
     def __lt__(self, other):
@@ -66,16 +70,16 @@ class Token:
                      deprel if deprel else deprel_copy,
                      deps if deps else deps_copy,
                      misc if misc else misc_copy)
-    
+
     def add_child(self, child):
         self._children_list.append(child)
-    
+
     def remove_child(self, child):
         self._children_list.remove(child)
-    
+
     def get_children(self):
         return self._children_list
-    
+
     def get_children_with_rels(self):
         return [(child, list(child.get_new_relations(self))[0][1]) for child in self.get_children()]
 
@@ -85,16 +89,16 @@ class Token:
         sorted_ = sorted((h, sorted(rels)) for h, rels in self.get_new_relations())
         self._conllu_info["deps"] = "|".join([str(a.get_conllu_field('id')) + ":" + bb.to_str(remove_enhanced_extra_info, remove_bart_extra_info) for (a, b) in sorted_ for bb in b])
         return "\t".join([str(v) for v in self._conllu_info.values()])
-    
+
     def set_conllu_field(self, field, val):
         self._conllu_info[field] = val
-    
+
     def get_conllu_field(self, field):
         return self._conllu_info[field]
 
     def get_parents(self):
         return self._new_deps.keys()
-    
+
     def get_new_relations(self, given_head=None):
         if given_head:
             if given_head in self._new_deps:
@@ -113,7 +117,7 @@ class Token:
         else:
             self._new_deps[head] = [rel]
             head.add_child(self)
-    
+
     def remove_edge(self, rel, head):
         assert isinstance(rel, Label)
         if head in self._new_deps and rel in self._new_deps[head]:
@@ -121,14 +125,14 @@ class Token:
             if not self._new_deps[head]:
                 self._new_deps.pop(head)
                 head.remove_child(self)
-    
+
     def remove_all_edges(self):
         _ = [self.remove_edge(edge, head) for head, edges in list(self.get_new_relations()) for edge in edges]
-    
+
     def replace_edge(self, old_rel, new_rel, old_head, new_head):
         self.remove_edge(old_rel, old_head)
         self.add_edge(new_rel, new_head)
-    
+
     # operator overloading: less than
     def __lt__(self, other):
         return self.get_conllu_field('id') < other.get_conllu_field('id')
