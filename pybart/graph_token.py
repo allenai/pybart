82ea5ac8212970fb4895f0a86bd39b339819b3c5
Aryeh Tiktinsky <aryehgigi@gmail.com>
2023-06-11 21:54:11 +0300

    Revert "Reduce eud literals"

--- pybart/graph_token.py
+++ pybart/graph_token.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass, field
-from .constants.constants import EUD_LITERAL_ALLOWED_LIST
 
 
 @dataclass
@@ -31,9 +30,7 @@ class Label:
         eud = ""
         if self.eud is not None:
             if not remove_enhanced_extra_info:
-                if self.eud.lower() not in EUD_LITERAL_ALLOWED_LIST:
-                    self.eud = "_other"
-                eud = ":" + self.eud.lower()
+                eud = ":" + self.eud
 
         bart = ""
         if self.src is not None:
